8f68a91ede7b80627a0ffa27980b9fbcc3001720
MrNivox <85137683+MrNivox@users.noreply.github.com>
2025-07-13 23:35:25 -0400

    temp ui and backend operations

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 
 
-
+const display = document.getElementById("display")
+let SignClick = false;
 let N1;
 let N2;
 let Sign;
@@ -19,13 +20,11 @@ function Multiply(x,y)
  return x * y
  
 }
-
 function Divide (x,y)
 {
  return x / y
  
 }
-
 function Operate(x ,y ,sign)
 {
    if(sign ==  "+"){return Additon(x,y)}
@@ -34,8 +33,30 @@ function Operate(x ,y ,sign)
    if(sign ==  "*"){return Multiply(x,y)}
    else {return "ERROR!"}
 }
+if(SignClick == true){
+SignClick = false
+if(N1 == null){N1 = display.innerText}
+else{N2 = display.innerText}
 
 
-console.log(Multiply(userX,userY))
-Substraction(5,2)
-Additon(3,2)
+
+}
+ document.getElementById("*").onclick = function () { SignClick = true}
+ document.getElementById("-").onclick = function () {SignClick = true}
+ document.getElementById("+").onclick = function () {SignClick = true}
+
+ document.getElementById("=").onclick = function () {Operate(N1,N2,Sign) }
+ document.getElementById("1").onclick = function () {display.innerText = "1"}
+ document.getElementById("2").onclick = function () {display.innerText = "2"}
+ document.getElementById("3").onclick = function () {display.innerText = "3"}
+ document.getElementById("4").onclick = function () {display.innerText = "4"}
+ document.getElementById("5").onclick = function () {display.innerText = "5"}
+ document.getElementById("6").onclick = function () {display.innerText = "6"}
+ document.getElementById("7").onclick = function () {display.innerText = "7"}
+ document.getElementById("8").onclick = function () {display.innerText = "8"}
+ document.getElementById("9").onclick = function () {display.innerText = "9"}
+ document.getElementById("clear").onclick = function One() {display.innerText = "0000"}
+
+
+
+
