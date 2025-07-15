5b52501b1cf88f6ba9f67b6beb84eb4f6f25e00d
MrNivox <85137683+MrNivox@users.noreply.github.com>
2025-07-14 21:39:37 -0400

    back end operations math
    fucntion for operations and buttons working

--- calculator.js
+++ calculator.js
@@ -2,9 +2,10 @@
 
 const display = document.getElementById("display")
 let SignClick = false;
-let N1;
-let N2;
+let N1 = null;
+let N2 = null;
 let Sign;
+let tempDisplay;
 
 function Additon(x,y)
 {
@@ -27,35 +28,58 @@ function Divide (x,y)
 }
 function Operate(x ,y ,sign)
 {
-   if(sign ==  "+"){return Additon(x,y)}
-   if(sign ==  "-"){return Substraction(x,y)}
-   if(sign ==  "/"){return Divide(x,y)}
-   if(sign ==  "*"){return Multiply(x,y)}
-   else {return "ERROR!"}
+   let result;
+   if(sign ==  "+"){y = Additon(x,y)}
+   if(sign ==  "-"){y = Substraction(x,y)}
+   if(sign ==  "/"){y = Divide(x,y)}
+   if(sign ==  "*"){y = Multiply(x,y)}
+
+   display.innerText = y
 }
-if(SignClick == true){
-SignClick = false
-if(N1 == null){N1 = display.innerText}
-else{N2 = display.innerText}
+function Check() {
 
-
+ if(SignClick == true){
+   SignClick = false
+   
+   N1 = tempDisplay
+   console.log(N1)
+   
+   
+ }
 
 }
- document.getElementById("*").onclick = function () { SignClick = true}
- document.getElementById("-").onclick = function () {SignClick = true}
- document.getElementById("+").onclick = function () {SignClick = true}
 
- document.getElementById("=").onclick = function () {Operate(N1,N2,Sign) }
- document.getElementById("1").onclick = function () {display.innerText = "1"}
- document.getElementById("2").onclick = function () {display.innerText = "2"}
- document.getElementById("3").onclick = function () {display.innerText = "3"}
- document.getElementById("4").onclick = function () {display.innerText = "4"}
- document.getElementById("5").onclick = function () {display.innerText = "5"}
- document.getElementById("6").onclick = function () {display.innerText = "6"}
- document.getElementById("7").onclick = function () {display.innerText = "7"}
- document.getElementById("8").onclick = function () {display.innerText = "8"}
- document.getElementById("9").onclick = function () {display.innerText = "9"}
+function removeSign(str) {
+
+    return str.replace("*" | "/",'');
+}
+
+function removeLeadingZerosRegex(str) {
+    return str.replace(/^0+(?=\d)/, '');
+}
+
+ document.getElementById("/").onclick = function () {Sign = "/"; SignClick = true;tempDisplay = display.innerText ;display.innerText = "/"; Check()}
+ document.getElementById("*").onclick = function () { Sign = "*";SignClick = true; tempDisplay = display.innerText ;display.innerText = "*"; Check()}
+ document.getElementById("-").onclick = function () {Sign = "-";SignClick = true;tempDisplay = display.innerText ;display.innerText = "-"; Check()}
+ document.getElementById("+").onclick = function () {Sign = "+";SignClick = true;tempDisplay = display.innerText ;display.innerText = "+"; Check()}
+ document.getElementById("=").onclick = function () {N2 = display.innerText ;console.log(Operate(N1,N2,Sign)) }
+
+ document.getElementById("1").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "1";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("2").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "2";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("3").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "3";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("4").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "4";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("5").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "5";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("6").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "6";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("7").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "7";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("8").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "8";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("9").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "9";display.innerText = removeLeadingZerosRegex(display.innerText)}
+ document.getElementById("0").onclick = function () {display.innerText = removeSign(display.innerText);display.innerText = display.innerText + "0";display.innerText = removeLeadingZerosRegex(display.innerText)}
  document.getElementById("clear").onclick = function One() {display.innerText = "0000"}
+  
+
+
+ 
+
 
 
 
